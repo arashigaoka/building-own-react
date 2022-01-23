6061ba004573c8e1aa081048ded1bbf5aa83490f
arashigaoka <17226903+arashigaoka@users.noreply.github.com>
2022-01-23 11:42:01 +0900

    add fiber

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import { re } from 'semver';
+
 const Didact = {
   createElement,
   render,
@@ -8,6 +10,13 @@ type DidactElement = {
   props: {
     nodeValue?: string;
     children?: Array<DidactElement>;
+  };
+};
+
+type Fiber = {
+  dom: Text | HTMLElement;
+  props: {
+    children: Array<DidactElement>;
   };
 };
 
@@ -42,20 +51,88 @@ function createTextElement(text) {
   };
 }
 
-function render(element: DidactElement, container) {
+function createDom(fiber: DidactElement) {
   const dom =
-    element.type === 'TEXT_ELEMENT'
+    fiber.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   const isProperty = (key) => key !== 'children';
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+  return dom;
+}
+
+function render(element: DidactElement, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+  // element.props.children.forEach((child) => render(child, dom));
+  // container.appendChild(dom);
+}
+
+let nextUnitOfWork: Fiber | null = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  // add dom
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendchild(fiber.dom);
+  }
+
+  // add fiber tree
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+  // search next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const container = document.getElementById('root');
